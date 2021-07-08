b102a68614425b871326c9efa50bf4db53269ee9
Jacob Alford <jacob.alford@me.com>
2021-07-08 12:00:22 -0600

    Add some stuff lol

--- src/CrudBuckets/Api.hs
+++ src/CrudBuckets/Api.hs
@@ -1,4 +1,24 @@
 module CrudBuckets.Api (Api) where
 import Servant.API
 
-type Api = EmptyAPI
+data Comment = Comment {
+  id :: String,
+  body :: String,
+  user :: User,
+  user_id :: String,
+  parent_id :: Maybe String,
+  parent_comment :: Maybe Comment,
+  child_comments :: [Comment],
+  post_id :: String
+                       }
+
+data User = User {
+  id :: String,
+  email :: String,
+  password :: String,
+  display_name :: String,
+  curernt_refresh_token :: Maybe String,
+  comments :: [Comment]
+                 }
+
+type Api = "users" :> Get '[JSON] [User]
