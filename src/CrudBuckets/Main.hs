4293a8f4cd7dbbb38f0bfe547350659e3c4a8148
Jacob Alford <jacob.alford@me.com>
2022-02-11 11:15:08 -0700

    Finish put schema, update TODO

--- src/CrudBuckets/Main.hs
+++ src/CrudBuckets/Main.hs
@@ -1,11 +1,13 @@
 module CrudBuckets.Main (main) where
 
-import Data.Proxy (Proxy (Proxy))
-import Servant.Server
-import Network.Wai.Handler.Warp (run)
-
+import Control.Concurrent (newMVar)
 import CrudBuckets.Api (Api)
 import CrudBuckets.Server (server)
+import Data.Proxy (Proxy (Proxy))
+import Network.Wai.Handler.Warp (run)
+import Servant.Server
 
 main :: IO ()
-main = run 8080 (serve (Proxy :: Proxy Api) server)
+main = do
+  buckets <- newMVar mempty
+  run 8080 (serve (Proxy :: Proxy Api) $ server buckets)
--- src/CrudBuckets/Server.hs
+++ src/CrudBuckets/Server.hs
@@ -1,43 +1,55 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module CrudBuckets.Server (server) where
+
 import Control.Concurrent
+import Control.Monad.Except (throwError)
 import Control.Monad.IO.Class
-import Servant.API
-import Servant.Server
-import Data.Aeson (Value)
 import CrudBuckets.Api (Api, Schema)
-import Data.Text (Text)
+import Data.Aeson (Value)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Text (Text)
+import Servant.API
+import Servant.Server
 
-data Bucket = Bucket {
-    schema :: Schema,
+data Bucket = Bucket
+  { schema :: Schema,
     values :: Map Text Value
   }
 
-newtype Buckets = Buckets {
-  getBuckets :: Map Text Bucket
-}
+newtype Buckets = Buckets
+  { unBuckets :: Map Text Bucket
+  }
+  deriving newtype (Semigroup, Monoid)
 
 {-
   TODO
-    1. Find someway to persist these buckets
-      1. MVar State
+    1. --Find someway to persist these buckets--
+      1. --MVar State--
     2. Rest of the routes
+      1. Validate schema
+      2. Validate body with schema
+    3. Logging
  -}
 
 updateIfEmpty :: Buckets -> Text -> Schema -> Maybe Buckets
-updateIfEmpty buckets key schema = 
-  case Map.lookup key $ getBuckets buckets of
-    Nothing -> Just $ Buckets $ Map.insert t bucket (getBuckets a)
-    Just _ -> Nothing
+updateIfEmpty buckets key schema =
+  let bucket = Bucket {schema = schema, values = mempty}
+   in case Map.lookup key $ unBuckets buckets of
+        Nothing -> Just $ Buckets $ Map.insert key bucket (unBuckets buckets)
+        Just _ -> Nothing
 
 putSchema :: MVar Buckets -> Text -> Schema -> Handler NoContent
-putSchema buckets t v = do 
-  let bucket = Bucket { schema = v, values = mempty }
-  let failOrNot = do
-    buckets <- buckets
-  liftIO $ modifyMVar_ buckets (\a -> pure $ updateIfEmpty)
-  pure NoContent
+putSchema buckets key schema = do
+  updated <- liftIO $ modifyMVar buckets updateBuckets
+  if updated then pure NoContent else throwError err409
+  where
+    updateBuckets :: Buckets -> IO (Buckets, Bool)
+    updateBuckets buckets = case updateIfEmpty buckets key schema of
+      Just newBuckets -> pure (newBuckets, True)
+      Nothing -> pure (buckets, False)
 
-server :: Server Api
-server = undefined :<|> undefined :<|> undefined :<|> putSchema undefined
+server :: MVar Buckets -> Server Api
+server buckets = undefined :<|> undefined :<|> undefined :<|> putSchema buckets
