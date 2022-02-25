fb1e7f3e1169d2a13bf53820b6bc81d1fc6934a4
Jacob Alford <jacob.alford@me.com>
2022-02-25 10:56:40 -0700

    Finish routes

--- src/CrudBuckets/Server.hs
+++ src/CrudBuckets/Server.hs
@@ -28,10 +28,12 @@ newtype Buckets = Buckets
   TODO
     1. --Find someway to persist these buckets--
       1. --MVar State--
-    2. Rest of the routes
+    2. --Rest of the routes--
       1. Validate schema
-      2. Validate body with schema
+      2. Validate body-values with schema
     3. Logging
+    4. Switch to Servant-Generics
+    5. Persist MVar to disk
  -}
 
 updateIfEmpty :: Buckets -> Text -> Schema -> Maybe Buckets
@@ -51,5 +53,33 @@ putSchema buckets key schema = do
       Just newBuckets -> pure (newBuckets, True)
       Nothing -> pure (buckets, False)
 
+putValue :: MVar Buckets -> Text -> Text -> Value -> Handler NoContent
+putValue buckets bucketKey key jsonValue = do
+  updated <- liftIO $ modifyMVar buckets updateBuckets
+  if updated then pure NoContent else throwError err404
+  where
+    updateBuckets :: Buckets -> IO (Buckets, Bool)
+    updateBuckets buckets = case Map.lookup bucketKey (unBuckets buckets) of
+      Nothing -> pure (buckets, False)
+      Just bucket -> do
+        let newBucket = bucket {values = Map.insert key jsonValue (values bucket)}
+        pure (Buckets $ Map.insert bucketKey newBucket (unBuckets buckets), True)
+
+getValue :: MVar Buckets -> Text -> Text -> Handler Value
+getValue mvarBuckets bucketKey key = do
+  buckets <- liftIO $ readMVar mvarBuckets
+  case Map.lookup bucketKey (unBuckets buckets) of
+    Nothing -> throwError err404
+    Just bucket -> case Map.lookup key (values bucket) of
+      Nothing -> throwError err404
+      Just value -> pure value
+
+getBucketKeys :: MVar Buckets -> Text -> Handler [Text]
+getBucketKeys mvarBuckets bucketKey = do
+  buckets <- liftIO $ readMVar mvarBuckets
+  case Map.lookup bucketKey (unBuckets buckets) of
+    Nothing -> throwError err404
+    Just bucket -> pure $ Map.keys (values bucket)
+
 server :: MVar Buckets -> Server Api
-server buckets = undefined :<|> undefined :<|> undefined :<|> putSchema buckets
+server buckets = getValue buckets :<|> putValue buckets :<|> getBucketKeys buckets :<|> putSchema buckets
