e1daadd990b47041757805c784a5c29572b945da
Jacob Alford <jacob.alford@me.com>
2022-03-30 13:33:32 -0600

    Add logging

--- src/CrudBuckets/Main.hs
+++ src/CrudBuckets/Main.hs
@@ -1,13 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CrudBuckets.Main (main) where
 
 import Control.Concurrent (newMVar)
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.Logger.CallStack (MonadLogger, logDebug, logError, logInfo)
 import CrudBuckets.Api (Api)
 import CrudBuckets.Server (server)
 import Data.Proxy (Proxy (Proxy))
+import qualified Data.Text as Text
+import Network.Wai (Middleware)
 import Network.Wai.Handler.Warp (run)
 import Servant.Server
+
+loggingMiddleware :: Middleware
+loggingMiddleware app req respond = do
+  app req $ \res -> do
+    runStdoutLoggingT $ logInfo $ "Request: " <> Text.pack (show req)
+    respond res
 
 main :: IO ()
 main = do
   buckets <- newMVar mempty
-  run 8080 (serve (Proxy :: Proxy Api) $ server buckets)
+  run 8080 (loggingMiddleware $ serve (Proxy :: Proxy Api) $ hoistServer (Proxy :: Proxy Api) runStdoutLoggingT $ server buckets)
--- src/CrudBuckets/Server.hs
+++ src/CrudBuckets/Server.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module CrudBuckets.Server (server) where
 
 import Control.Concurrent
-import Control.Monad.Except (throwError)
+import Control.Monad.Except (MonadError, throwError)
 import Control.Monad.IO.Class
+import Control.Monad.Logger (LoggingT)
+import Control.Monad.Logger.CallStack (MonadLogger, logDebug, logError, logInfo)
 import CrudBuckets.Api (Api, Schema)
 import Data.Aeson (Value)
 import Data.Map (Map)
@@ -43,7 +47,7 @@ updateIfEmpty buckets key schema =
         Nothing -> Just $ Buckets $ Map.insert key bucket (unBuckets buckets)
         Just _ -> Nothing
 
-putSchema :: MVar Buckets -> Text -> Schema -> Handler NoContent
+putSchema :: (MonadIO m, MonadError ServerError m) => MVar Buckets -> Text -> Schema -> m NoContent
 putSchema buckets key schema = do
   updated <- liftIO $ modifyMVar buckets updateBuckets
   if updated then pure NoContent else throwError err409
@@ -53,7 +57,7 @@ putSchema buckets key schema = do
       Just newBuckets -> pure (newBuckets, True)
       Nothing -> pure (buckets, False)
 
-putValue :: MVar Buckets -> Text -> Text -> Value -> Handler NoContent
+putValue :: (MonadIO m, MonadError ServerError m) => MVar Buckets -> Text -> Text -> Value -> m NoContent
 putValue buckets bucketKey key jsonValue = do
   updated <- liftIO $ modifyMVar buckets updateBuckets
   if updated then pure NoContent else throwError err404
@@ -65,21 +69,26 @@ putValue buckets bucketKey key jsonValue = do
         let newBucket = bucket {values = Map.insert key jsonValue (values bucket)}
         pure (Buckets $ Map.insert bucketKey newBucket (unBuckets buckets), True)
 
-getValue :: MVar Buckets -> Text -> Text -> Handler Value
+getValue :: (MonadIO m, MonadError ServerError m, MonadLogger m) => MVar Buckets -> Text -> Text -> m Value
 getValue mvarBuckets bucketKey key = do
   buckets <- liftIO $ readMVar mvarBuckets
   case Map.lookup bucketKey (unBuckets buckets) of
     Nothing -> throwError err404
     Just bucket -> case Map.lookup key (values bucket) of
-      Nothing -> throwError err404
+      Nothing -> do
+        logError $ "No value for key " <> key <> " in bucket " <> bucketKey
+        throwError err404
       Just value -> pure value
 
-getBucketKeys :: MVar Buckets -> Text -> Handler [Text]
+getBucketKeys :: (MonadIO m, MonadError ServerError m, MonadLogger m) => MVar Buckets -> Text -> m [Text]
 getBucketKeys mvarBuckets bucketKey = do
+  logInfo $ "Getting keys for bucket: " <> bucketKey
   buckets <- liftIO $ readMVar mvarBuckets
   case Map.lookup bucketKey (unBuckets buckets) of
-    Nothing -> throwError err404
+    Nothing -> do
+      logError $ "Bucket not found: " <> bucketKey
+      throwError err404
     Just bucket -> pure $ Map.keys (values bucket)
 
-server :: MVar Buckets -> Server Api
+server :: MVar Buckets -> ServerT Api (LoggingT Handler)
 server buckets = getValue buckets :<|> putValue buckets :<|> getBucketKeys buckets :<|> putSchema buckets
